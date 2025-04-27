3c041b35c9c23028b6fbb0d378ff6cc234b5cb71
Vijay Gupta <vijayg963@gmail.com>
2025-04-27 23:51:25 +0530

    add api fixes 2

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose';
 
-const username = process.env.NEXT_PUBLIC_MONGO_USERNAME;
-const password = process.env.NEXT_PUBLIC_MONGO_PASSWORD;
+const username =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGO_USERNAME
+    : process.env.NEXT_PUBLIC_MONGO_USERNAME;
+const password =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGO_PASSWORD
+    : process.env.NEXT_PUBLIC_MONGO_PASSWORD;
 const MONGODB_URI = `mongodb+srv://${username}:${password}@cluster0.i2hab2t.mongodb.net/contact_us?retryWrites=true&w=majority`;
 
 if (!MONGODB_URI) {
@@ -9,7 +15,7 @@ if (!MONGODB_URI) {
     'Please define the MONGODB_URI environment variable in your .env file'
   );
 }
-console.log("MONGODB_URI", MONGODB_URI);
+console.log('MONGODB_URI', MONGODB_URI);
 interface MongooseCache {
   conn: typeof mongoose | null;
   promise: Promise<typeof mongoose> | null;
@@ -19,7 +25,7 @@ interface MongooseCache {
 let cached: MongooseCache = global.mongoose || { conn: null, promise: null };
 
 if (!cached) {
-    // @ts-expect-error: global.mongoose might not be defined initially
+  // @ts-expect-error: global.mongoose might not be defined initially
   cached = global.mongoose = { conn: null, promise: null };
 }
 
@@ -29,11 +35,9 @@ async function connectToDatabase() {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URI)
-      .then((mongoose) => {
-        return mongoose;
-      });
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => {
+      return mongoose;
+    });
   }
   cached.conn = await cached.promise;
   return cached.conn;
